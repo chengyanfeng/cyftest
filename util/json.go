053077c51f0a23a046fbce5e3d2c351222e4fe72
16141 <569498977@qq.com>
2018-04-17 11:35:04 +0800

    添加

--- util/json.go
+++ util/json.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/clbanning/mxj"
 )
-
+//1.json字符串转换P类型
 func JsonDecode(b []byte) (p *P) {
 	p = &P{}
 	err := json.Unmarshal(b, p)
@@ -14,7 +14,7 @@ func JsonDecode(b []byte) (p *P) {
 	}
 	return
 }
-
+//2.interface类型转换成json格式的字符串
 func JsonEncode(v interface{}) (r string) {
 	b, err := json.Marshal(v)
 	if err != nil {
@@ -23,24 +23,24 @@ func JsonEncode(v interface{}) (r string) {
 	r = string(b)
 	return
 }
-
+//3.判断是不是json
 func IsJson(b []byte) bool {
 	var j json.RawMessage
 	return json.Unmarshal(b, &j) == nil
 }
-
+//4.解析json数组成为P类型
 func JsonDecodeArray(b []byte) (p []P, e error) {
 	p = []P{}
 	e = json.Unmarshal(b, &p)
 	return
 }
-
+//5.解析json为数组
 func JsonDecodeArray_str(b []byte) (p []string, e error) {
 	p = []string{}
 	e = json.Unmarshal(b, &p)
 	return
 }
-
+//6.解析json数组为P
 func JsonDecodeArrays(b []byte) (p *[]P) {
 	p = &[]P{}
 	e := json.Unmarshal(b, p)
@@ -49,7 +49,7 @@ func JsonDecodeArrays(b []byte) (p *[]P) {
 	}
 	return
 }
-
+//7.解析json为数组
 func JsonDecodeStrings(s string) (r []string) {
 	r = []string{}
 	e := json.Unmarshal([]byte(s), &r)
@@ -58,14 +58,14 @@ func JsonDecodeStrings(s string) (r []string) {
 	}
 	return
 }
-
+//8.解析多个参数为字符串
 func JoinStr(val ...interface{}) (r string) {
 	for _, v := range val {
 		r += ToString(v)
 	}
 	return
 }
-
+//解析xml为字符串
 func Xml2Json(src string) (s string, err error) {
 	m, err := mxj.NewMapXml([]byte(src))
 	return JsonEncode(m), err
